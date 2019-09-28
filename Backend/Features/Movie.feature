ea57114aa3e776bf26fa685ccee40a4ca6c386a2
Manish Goyal <manish.goyal02@nagarro.com>
2019-09-28 14:13:10 +0530

    Part 1

---
+++ Backend/Features/Movie.feature
@@ -0,0 +1,14 @@
+﻿Feature: Movie
+
+@mytag
+Scenario Outline: Add Movie
+	Given I logged in as (<UserName>,<Password>)
+	And ModelState is correct
+	Then the system should return <StatusCode>
+
+Examples: 
+	| Title    | Description  | Image | Director   | StatusCode | Id  | UserName | Password |
+	| Rockstar | Sampal Movie | image | Imtiaz Ali | 201        |  1  | admin    | password |
+	
+	
+
---
+++ Backend/Steps/LoginSteps.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using WhatsTheNameAPI.AcceptanceTests.Helpers;
+using WhatsTheNameAPI.AcceptanceTests.Model.Entities;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+using TechTalk.SpecFlow;
+using System.Linq;
+using PropertiesAPI.AcceptanceTests.Model.Entities;
+
+namespace WhatsTheNameAPI.AcceptanceTests.Steps
+{
+    public class LoginSteps
+    {
+        private Login _login;
+        private IRestResponse _restResponse;
+        private HttpStatusCode _statusCode;
+        private LoginResponse _loginResponse;
+
+        [Given(@"I logged in as \((.*),(.*)\)")]
+        public void GivenILogedInAs(string username, string password)
+        {
+            _login = new Login()
+            {
+                Username= username,
+                Password=password
+            };
+
+            var request = new HttpRequestWrapper()
+                            .SetMethod(Method.POST)
+                            .SetResourse("/api/properties/")
+                            .AddJsonContent(_login);
+
+            _restResponse = new RestResponse();
+            _restResponse = request.Execute();
+            _statusCode = _restResponse.StatusCode;
+            _loginResponse=JsonConvert.DeserializeObject<LoginResponse>(_restResponse.Content);
+            ScenarioContext.Current.Add("Login", _login);
+        }
+    }
+}
